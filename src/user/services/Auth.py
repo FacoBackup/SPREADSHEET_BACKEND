2a4a1a8d873992f22a1afc0741e6ba520d38e32e
facobackup <gustavomicaelbarbosa@gmail.com>
2021-02-02 11:17:42 -0300

    Tweaks

--- src/user/services/Auth.py
+++ src/user/services/Auth.py
@@ -1,21 +1,22 @@
 # from rest_framework_simplejwt.tokens import AccessToken
 from src.user.services import UserRead
-import json
+from ldap3 import Server, Connection, ALL
 import jwt
+import django_python3_ldap
 import time as time_
 from rest_framework import status
 
-
-def sign_in(user_field):
-    found = UserRead.search_user(user_field)
-    if found is not None:
-
-        user = json.loads(found)
-        email = user[0]['fields']['email']
-        encoded_jwt = jwt.encode({"user_email": email, 'exp': (time_.time()) + 1000 * 60 * 10},
+def sign_in(user_id, password):
+    user = UserRead.UserReadService.read_user_by_id(user_id)
+    if user is not None:
+        server = Server('ldap://')
+        encoded_jwt = jwt.encode({"user_id": user.id, 'exp': (time_.time() + 1000 * 60 * 10)},
                                  "askdasdiuh123i1y98yejas9d812hiu89dqw9", algorithm="HS256")
 
-        return encoded_jwt
+        return {
+            "JWT": encoded_jwt,
+            "ID": user.id
+        }
 
     else:
         return status.HTTP_404_NOT_FOUND
